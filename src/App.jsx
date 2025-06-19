3d175c0a26cf86a3cac8830d4df28756d9bab85f
yuricifiello <yuricifiello@gmail.com>
2025-06-19 02:14:21 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/layouts/Header";
+import Main from "./components/layouts/Main";
+import Footer from "./components/layouts/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
---
+++ src/components/layouts/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <main>
+      <h1>App React</h1>
+    </main>
+  );
+}
