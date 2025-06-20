b244374178cbf95f43839c0c98acc0cac649212a
yuricifiello <yuricifiello@gmail.com>
2025-06-20 14:41:35 +0200

    add loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+export default function Loader() {
+  return (
+    <div className="loader-overlay">
+      <div className="spinner-border text-primary" role="status">
+        <span className="visually-hidden">Caricamento...</span>
+      </div>
+    </div>
+  );
+}
