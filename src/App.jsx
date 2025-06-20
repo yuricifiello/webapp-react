e4eda83f060e7b3fe7ae9952e82fa5b4d558e884
yuricifiello <yuricifiello@gmail.com>
2025-06-20 14:41:22 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
+import { LoaderProvider, useLoader } from "./context/LoaderContext";
+import Loader from "./components/Loader";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MoviePage from "./pages/MoviePage";
 
+function LoaderWrapper() {
+  const { isLoading } = useLoader();
+  return isLoading ? <Loader /> : null;
+}
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies/:id" element={<MoviePage />} />
-      </Routes>
-    </BrowserRouter>
+    <LoaderProvider>
+      <BrowserRouter>
+        <LoaderWrapper />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies/:id" element={<MoviePage />} />
+        </Routes>
+      </BrowserRouter>
+    </LoaderProvider>
   );
 }
 
